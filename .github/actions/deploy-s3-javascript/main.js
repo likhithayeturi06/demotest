cb0b629138957a04a2aa0b59fa72fd49087ccde1
Likhitha Yeturi <121950026+likhithayeturi06@users.noreply.github.com>
2023-02-17 12:12:15 +0000

    created custom javascript action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+function run() {
+    core.notice('Custom Javascript action');
+    
+
+
+
+
+}
+
+
+run();
