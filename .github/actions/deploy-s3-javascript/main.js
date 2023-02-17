4cf4a95bd1b25d7e5a20f8b4c67be758abea0f2c
Likhitha Yeturi <121950026+likhithayeturi06@users.noreply.github.com>
2023-02-17 14:16:43 +0000

    javascript action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,13 +3,23 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+
+    // getting input values
+    const bucket = core.getInput('bucket',{ required:true});
+    const bucketregion = core.getInput('bucket-region',{ required:true});
+    const distfolder = core.getInput('dist-folder',{ required:true});
+
+    //upload files
+    //const localfolder = distfolder
+    const s3Uri = `s3:://${bucket}`
+
+    AWS_ACCESS_KEY_ID = //copied access key
+    exec.exec(`aws s3 sync <distfolder> <s3Uri> --region ${bucketregion}`)
+
+    const websiteurl = `http://${bucket}.s3-website-${bucketregion}.amazonaws.com`;
+    core.setOutput('website-url', websiteurl);  //::set-output
+
     core.notice('Custom Javascript action');
-    
-
-
-
 
 }
-
-
 run();
